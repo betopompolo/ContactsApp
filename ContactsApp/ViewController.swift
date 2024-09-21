2260054390fd20701befff817ed4ce38ed68e957
Adalberto Pompolo <apompolo@madison-reed.com>
2024-09-21 09:19:06 -0300

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Adalberto Nassu Pompolo on 21/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
