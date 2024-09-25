7ff500e2c9457732a9199ab5790c748701d03dd0
Adalberto Pompolo <apompolo@madison-reed.com>
2024-09-25 11:30:29 -0300

    initial commit

--- ContactsApp/ViewController.swift
+++ ContactsApp/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Contact {
+    var name: String
+    var phone: String
+    var email: String
+    var companyName: String
+}
 
+let mockContact = Contact(
+    name: "Adalberto Nassu Pompolo",
+    phone: "1-770-736-8031",
+    email: "beto.nassu@gmail.com",
+    companyName: "Agile Engine"
+)
+
+class ViewController: UIViewController {
+    @IBOutlet weak var initialsCircle: UIView!
+    @IBOutlet weak var initialsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initialsCircle.layer.borderWidth = 1
+        initialsCircle.layer.borderColor = UIColor.white.cgColor
+        initialsLabel.text = getInitials(from: mockContact.name)
+        
     }
-
-
+    
+    private func getInitials(from name: String) -> String {
+        let words = name.split(separator: " ")
+        
+        let firstName = words.first!.description
+        let lastName = words.last!.description
+        
+        return firstName.first!.description + lastName.first!.description
+    }
 }
 
